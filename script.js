e22183ede5ac077eb77a3f46485b1d6e4730bc95
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-12 13:46:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// DOM Elements
+const bodyElement = document.querySelector("body");
+
+async function fetchOffers() {
+  try {
+    //Récupère les données de l'url (API) et les transforme en JSON
+    const response = await fetch("https://thesimpsonsapi.com/api/characters");
+    const characters = await response.json();
+
+    // Affiche les données dans le DOM
+    characters.results.forEach((character) => {
+      const newH2Element = document.createElement("h2");
+      newH2Element.textContent = `Nom : ${character.name}`;
+      const newP = document.createElement("p");
+      newP.textContent = `Phrases culte : ${character.phrases.join(", ")}`;
+      bodyElement.appendChild(newH2Element);
+      bodyElement.appendChild(newP);
+    });
+  } catch (error) {
+    console.log("Erreur");
+  }
+}
+
+fetchOffers();
